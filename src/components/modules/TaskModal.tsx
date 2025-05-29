32e28566629f1b92a3a36802f8c526d1faf57642
ruhitbaidya <ruhitbaidya01@gmail.com>
2025-05-30 00:30:34 +0600

    this proejct some work

---
+++ src/components/modules/TaskModal.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { useForm } from "react-hook-form";
+import { Input } from "../ui/input";
+
+export function TaskModal() {
+  const form = useForm();
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Add Task</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Task</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter Your Name" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel />
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="duedate"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel />
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="priority"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel />
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import TaskCard from "@/components/modules/TaskCard";
+import { TaskModal } from "@/components/modules/TaskModal";
 import { useAppSelector } from "@/hooks/hooks";
 import { taskState } from "@/redux/fetcher/tasks";
 import type { ITasks } from "@/type";
@@ -8,6 +9,9 @@ const Home = () => {
   console.log(tasks);
   return (
     <div>
+      <div className="flex justify-end w-[70%] mx-auto mb-[20px]">
+        <TaskModal />
+      </div>
       {tasks &&
         tasks?.map((item: ITasks) => <TaskCard key={item.id} {...item} />)}
     </div>
